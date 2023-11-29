662e6024db4f05a3f00cf1403eb0daa9653b06d2
Poobesh Guptaa <91111210+POOBESH-GUPTAA@users.noreply.github.com>
2023-11-29 11:54:57 +0530

    Add files via upload

--- src/org/ecom/OpenCartTesting.java
+++ src/org/ecom/OpenCartTesting.java
@@ -1,12 +1,13 @@
 package org.ecom;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OpenCartTesting {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\poobe\\eclipse-workspace\\E - Commerce Testing\\driver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.opencart.com/");
@@ -14,8 +15,14 @@ public class OpenCartTesting {
 		WebElement h1 = driver.findElement(By.xpath("//h1[text()='The best FREE and open-source eCommerce platform']"));
 		String printH1 = h1.getText();
 		System.out.println(printH1);
-		WebElement reg = driver.findElement(By.xpath("(//a[text()='Login'])[2]"));
-		reg.click();
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+	    WebElement scrollDown = driver.findElement(By.xpath("//h2[text()='OpenCart Cloud']"));
+	    js.executeScript("arguments[0].scrollIntoView(true)", scrollDown);
+	    Thread.sleep(5000);
+		WebElement getStarted = driver.findElement(By.xpath("(//a[text()='Get Started'])[1]"));
+		getStarted.click();
+		
+		
 	}
 
 }
